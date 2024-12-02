4a7a79762593491a83f7f497a212d03017182f12
Eternatus9999 <eternatus9999@gmail.com>
2024-12-02 13:31:37 +0530

    Change the title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-violet-300 font-bold '>Welcome to N.E.X.T.S</h1>
+      <h1 className='text-5xl text-violet-300 font-bold '>Welcome to My World</h1>
     </main>
   )
 }

7598f6cec3a5a4f9924b4578177c4b4a6acbddc5
Eternatus9999 <eternatus9999@gmail.com>
2024-12-02 12:43:21 +0530

    Add Tailwind and make Tailwind configurations

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <h1 className='text-5xl text-violet-300 font-bold '>Welcome to N.E.X.T.S</h1>
+    </main>
   )
 }
 
